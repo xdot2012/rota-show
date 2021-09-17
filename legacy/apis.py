56ba165da2f0487a07303ca9f3649af5c14800bd
hawkthief <daniellbl@hotmail.com>
2021-09-17 20:52:06 -0300

    GenerateRouteAPI Hawk

--- legacy/apis.py
+++ legacy/apis.py
@@ -1,3 +1,6 @@
+import requests
+import json
+import ortools
 from django.db import IntegrityError
 from rest_framework import viewsets, status
 from rest_framework.views import APIView
@@ -6,6 +9,9 @@ from .serializers import LocalSerializer
 from .models import Local
 from accounts.models import User
 from rest_framework.response import Response
+from ortools.constraint_solver import routing_enums_pb2
+from ortools.constraint_solver import pywrapcp
+
 
 
 class LocalViewSet(viewsets.ModelViewSet):
@@ -79,13 +85,121 @@ class LocalViewSet(viewsets.ModelViewSet):
 
 class GenerateRouteAPI(APIView):
     def post(self, request):
-        print("LÓGICA AQUI")
+
+        api_key = '&key=AIzaSyD3iW-BDcjxvxPpQIr-YxZLu7TrcJ7I5hc'
+
+        #initial_point_pk = 16
+        Initial_obj = []
+        obj_list = []
         initial_point_pk = request.data['initial_point_pk']
+        user_pk = request.user.pk
+        try:
+            local_list = Local.objects.filter(user=user_pk)
+        except:
+            raise ("FUDEU")
+        local_url = ""
+        distance_matrix = []
 
-        response = {
-            'route': [
+        for x in local_list:
+            if x.pk == initial_point_pk:
+                local_url = str(x.latitude) + ',' + str(x.longitude)
+        if local_url == "":
+            return Response(data="ponto inicial não cadastrado", status=status.HTTP_400_BAD_REQUEST)
+        for x in local_list:
+            if x.pk != initial_point_pk:
+                local_url = local_url + "|" + str(x.latitude) + ',' + str(x.longitude)
+                obj_list.append(x)
+            else:
+                Initial_obj.append(x)
 
-            ]
-        }
-        return Response(response, status=status.HTTP_200_OK)
-        
+        url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins="
+        url = url + local_url + "&destinations=" + local_url + api_key
+
+        #url = "https://maps.googleapis.com/maps/api/distancematrix/json?origins=40.6655101,-73.89188969999998|40.659569,-73.933783&destinations=40.6655101,-73.89188969999998|40.659569,-73.933783&key=AIzaSyD3iW-BDcjxvxPpQIr-YxZLu7TrcJ7I5hc"
+
+        payload = {}
+        headers = {}
+
+        api_response = requests.request("GET", url, headers=headers, data=payload)
+        json.loads(api_response.text)
+
+        local_len = len(local_list)
+        #local_len = 2
+
+        for x in range(local_len):
+            local_distance = []
+            for y in range(local_len):
+                local_distance.append(api_response.json()["rows"][x]["elements"][y]["distance"]["value"])
+            distance_matrix.append(local_distance)
+
+        def create_data_model():
+            """Stores the data for the problem."""
+            data = {}
+            data['distance_matrix'] = distance_matrix  # yapf: disable
+            data['num_vehicles'] = 1
+            data['depot'] = 0
+            return data
+
+        def print_solution(manager, routing, solution):
+            """Prints solution on console."""
+            index = routing.Start(0)
+            route_distance = 0
+            plan_output = []
+            plan_output.append([])
+            while not routing.IsEnd(index):
+                plan_output[0].extend(['{}'.format(manager.IndexToNode(index))])
+                previous_index = index
+                index = solution.Value(routing.NextVar(index))
+                route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
+            plan_output[0].extend(['{}'.format(manager.IndexToNode(index))])
+            plan_output.append(['{}'.format(route_distance)])
+            response = plan_output
+            return (response)
+
+
+        """Entry point of the program."""
+        # Instantiate the data problem.
+        data = create_data_model()
+
+        # Create the routing index manager.
+        manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']),
+                                               data['num_vehicles'], data['depot'])
+
+        # Create Routing Model.
+        routing = pywrapcp.RoutingModel(manager)
+
+        def distance_callback(from_index, to_index):
+            """Returns the distance between the two nodes."""
+            # Convert from routing variable Index to distance matrix NodeIndex.
+            from_node = manager.IndexToNode(from_index)
+            to_node = manager.IndexToNode(to_index)
+            return data['distance_matrix'][from_node][to_node]
+
+        transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+        # Define cost of each arc.
+        routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+        # Setting first solution heuristic.
+        search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+        search_parameters.first_solution_strategy = (
+            routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+        # Solve the problem.
+        solution = routing.SolveWithParameters(search_parameters)
+
+        if solution:
+            res = print_solution(manager, routing, solution)
+            route = str(Initial_obj[0].pk)
+            for x in res[0]:
+                if x != 0:
+                    route = route + str(obj_list[int(x)-1])
+
+            response = {
+                'route': str(route),
+                'distance': str(int(int(res[1][0])*1.60934)),
+            }
+            return Response(response, status=status.HTTP_200_OK)
+
+        return Response(data="Resultado Não encontrado", status=status.HTTP_424_FAILED_DEPENDENCY)
+
--- legacy/models.py
+++ legacy/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Local(models.Model):
-    name =  models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
     user = models.ForeignKey('accounts.User', on_delete=models.CASCADE)
     latitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
     longitude = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
