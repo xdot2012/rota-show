c414dd16fef67d233ac4a2694f991044403ec1f9
douglas <douglasgsales@gmail.com>
2021-09-20 14:24:13 -0300

    Tela Inicial

--- legacy/views.py
+++ legacy/views.py
@@ -5,8 +5,4 @@ from django.views import View
 
 class HomeView(View):
     def get(self, request):
-        if request.user.is_authenticated:
-            return render(request, "templates/home.html")
-
-        else:
-            return redirect(reverse_lazy('login'))
+        return render(request, "templates/home.html")
